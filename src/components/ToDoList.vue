a7b4eeac02bb0762337e10fa36361b0eb5bd7953
Khai <KhaiZhang@163.com>
2019-07-21 23:23:07 +0800

    [feat : show Todo List]

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page_container">
+  <div class="container">
+    <h2>Jquery To Do List</h2>
+    <p>Simple Todo List with adding and filter by diff status</p>
+    <input type="text" id="inputNum" name="inputNum">
+    <button>add</button>
+    <ol>
+      <li>
+        <input type="checkbox" >
+        <span>123</span>
+      </li>
+    </ol>
+    <p><span>all&nbsp;</span><span>active&nbsp;</span><span>complete</span></p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
