94efe1802b3dd28d72a52571d14c71f97582dc69
ZHANGKH <KhaiZhang@163.com>
2019-07-22 13:48:43 +0800

    [feat : add todolist edit]

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -8,8 +8,9 @@
     <button class="addbtn" @click="addItem()">Add</button>
     <ol class ="list" >
       <li v-for="(item,index) in show(itemList)" v-bind:key="index" :class="{rackA:index%2==0}" >
-        <input type="checkbox" @click="changeStatus(index)" v-bind:checked="item.isFinish">
-        <span v-bind:class="{itemIsFinish:item.isFinish}">{{item.content}}</span>
+        <input type="checkbox" @click="changeStatus(item)" v-bind:checked="item.isFinish" >
+        <span v-if="!item.isEdit" v-bind:class="{itemIsFinish:item.isFinish}"  v-on:dblclick="changeToEdit(item)" >{{item.content}}</span>
+        <input v-else  type="text" v-model="currentContent" v-on:keyup.enter="changeContent(item)">
       </li>
     </ol>
     <div class="page_bottom">
@@ -32,15 +33,18 @@ export default {
     return {
       itemList:[],
       inputValue:null,
-      selectTpye:'ALL'
+      selectTpye:'ALL',
+      currentContent:null
     }
   },
   methods :{
-    changeStatus:function(index){
-        this.itemList[index].isFinish = !this.itemList[index].isFinish;
+    changeStatus:function(item){
+        let result = this.itemList.find(element => element === item);
+        result.isFinish = !result.isFinish;
+        item.isEditable = !item.isEditable;
     },
     addItem:function(){
-      this.itemList.push({isFinish:false,content:this.inputValue});
+      this.itemList.push({isFinish:false,content:this.inputValue,isEdit:false,isEditable:true});
       this.inputValue=null;
     },
     changeList:function(evnet){
@@ -50,6 +54,18 @@ export default {
       if(this.selectTpye == 'ALL') return list;
       else if(this.selectTpye == 'Active') return list.filter(item => !item.isFinish);
       else return list.filter(item => item.isFinish);
+    },
+    changeToEdit:function(item){
+      let result = this.itemList.find(element => element === item);
+        if(result.isEditable){
+          result.isEdit =!result.item;
+          this.currentContent = result.content;
+        }    
+    },
+    changeContent:function(item){
+        let result = this.itemList.find(element => element === item);
+        result.content =this.currentContent;
+        result.isEdit = !result.isEdit;   
     }
   }
 }
