f9f76837ec9a12ec4268316eb40b1aff30013545
ZHANGKH <KhaiZhang@163.com>
2019-07-22 10:54:27 +0800

    [feat: add item and finish item checked]

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -4,12 +4,12 @@
     <div>
     <h2>Jquery To Do List</h2>
     <p class="tips">Simple Todo List with adding and filter by diff status</p>
-    <input type="text" id="inputNum" name="inputNum">
-    <button class="addbtn">Add</button>
+    <input type="text" id="inputNum" name="inputNum" v-model="inputValue">
+    <button class="addbtn" @click="addItem()">Add</button>
     <ol class ="list" >
-      <li v-for="(item,index) in 4" v-bind:key="index" :class="{rackA:item%2==0}">
-        <input type="checkbox" >
-        <span>{{index}}</span>
+      <li v-for="(item,index) in itemList" v-bind:key="index" :class="{rackA:index%2==0}" >
+        <input type="checkbox" @click="changeStatus(index)">
+        <span v-bind:class="{itemIsFinish:item.isFinish}">{{item.content}}</span>
       </li>
     </ol>
     <div class="page_bottom">
@@ -27,6 +27,34 @@ export default {
   name: 'ToDoList',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      itemList:[
+        {
+          isFinish : false,
+          content : "123"
+        },
+        {
+          isFinish : false,
+          content : "xxx"
+        },
+        {
+          isFinish : false,
+          content : "sdad"
+        },
+      ],
+      inputValue:null
+    }
+  },
+  methods :{
+    changeStatus(index){
+        this.itemList[index].isFinish = !this.itemList[index].isFinish;
+    },
+    addItem(){
+      this.itemList.push({isFinish:false,content:this.inputValue});
+      this.inputValue=null;
+    }
   }
 }
 </script>
